fdb7ff31552763edaf595e1c74281ef09eaa0b99
Duncan McGreggor <duncan.mcgreggor@rackspace.com>
2013-09-28 18:37:05 -0500

    Added docstrings.

--- src/example_rest_api/server.clj
+++ src/example_rest_api/server.clj
@@ -7,7 +7,8 @@
 
 
 (defn -start-server
-  "Run an HTTP server. This is useful in the REPL.
+  "Run an HTTP server. This is what ends up actually calling the http-kit
+  run-server function.
 
   The 'options' parameter is just a map. To run a dev server, the minimal
   otions necessary are:
@@ -26,7 +27,18 @@
   (-start-server {:development true :port port}))
 
 (defn new-manager [& [options server]]
-  ""
+  "This is an object that uses closures to keep track of server options and a
+  server instance (as ulimately returned by httpkit/run-server).
+
+  The use of nested anonymous functions for tracking state is somewhat abstruse.
+  However, the API for http-kit isn't such a good one, in this case. You likely
+  won't need to call this function ever, since convenience functions are
+  provided below. In the event that you wanted to use this function directly
+  from the REPL, you would do something like this:
+    => (def server-manager (new-manager))
+    => (def server-manager (start server-manager))
+    => (stop server-manager)
+  "
   (fn [command]
     (case command
       :start (fn []
@@ -42,23 +54,52 @@
                 options
                 (server))))))
 
-(defn do-server-action [server command]
+(defn -do-server-action [server command]
+  "This is used as a convenience function in the following functions to avoid
+  typing multiple 'apply's for every function that needs to call into the server
+  manager."
   (apply server [command]))
 
 (defn start [server]
-  (apply (do-server-action server :start) []))
+  "A function for starting a server.
+
+  Generally, a regular server will be started via the command-line with
+  command-line options. As such, no options are provided for this function."
+  (apply (-do-server-action server :start) []))
 
 (defn start-dev [server & {:keys [port] :or {:port config/default-port}}]
-  (apply (do-server-action server :start-dev) [port]))
+  "A function for starting a development server with an optional :port keyword."
+  (apply (-do-server-action server :start-dev) [port]))
 
 (defn stop [server]
-  (apply (do-server-action server :stop) []))
+  "A function for stoping a server.
+
+  The intent is to use this from the REPL:
+    => (def server (run-dev))
+    => (stop server)
+  "
+  (apply (-do-server-action server :stop) []))
 
 (defn run []
+  "Run a regular server. Any changes made to files while running this server
+  will not be apparent until the server is restarted.
+
+  The intent is to use this from the REPL:
+    => (def server (run))
+  "
   (let [server (new-manager {:port config/default-port})]
     (start server)))
 
 (defn run-dev
+  "Run a development server, with an optional :port keyword. This server will
+  start an http-kit server running in development (autoreload) mode. As such,
+  any changes to files will result in corresponding changes to the running
+  server.
+
+  The intent is to use this from the REPL:
+    => (def server (run-dev))
+    => (def server (run-dev 7080))
+  "
   ([]
     (run-dev config/default-port))
   ([port]
