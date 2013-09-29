b884a4840129139c22bc5b267fd43051822c3e90
Duncan McGreggor <duncan.mcgreggor@rackspace.com>
2013-09-29 17:28:21 -0500

    Added error checking to client.

--- src/example_rest_api/client.clj
+++ src/example_rest_api/client.clj
@@ -1,6 +1,14 @@
 (ns example-rest-api.client
-  (:require [clj-http.client :as http]))
+  (:require [clojure.tools.logging :as log]
+            [clj-http.client :as http])
+  (:import [java.net ConnectException]
+           [java.io EOFException]))
 
 
 (defn get-hello [base-url]
-  ((http/get (str base-url "/")) :body))
+  (try ((http/get (str base-url "/")) :body)
+    (catch ConnectException error
+      (log/error (.getMessage error)))
+    (catch EOFException error
+      (log/error (.getMessage error)))))
+
