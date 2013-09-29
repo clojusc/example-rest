e4a292d9baf4c3ba0caa9e7b601773406431c528
Duncan McGreggor <duncan.mcgreggor@rackspace.com>
2013-09-29 18:39:18 -0500

    Moved hello-world into API code.

---
+++ src/example_rest_api/api.clj
@@ -0,0 +1,8 @@
+(ns example-rest-api.api
+  (:require [clojure.tools.logging :as log]
+            [example-rest-api.config :as config]
+            [example-rest-api.util :as util]))
+
+
+(defn hello-world []
+  "Hello World!!!")
--- src/example_rest_api/routes.clj
+++ src/example_rest_api/routes.clj
@@ -1,10 +1,11 @@
 (ns example-rest-api.routes
   (:require [compojure.core :refer [GET defroutes]]
             [compojure.route :as route]
+            [example-rest-api.api :as api]
             [example-rest-api.const :as const]))
 
 
 (defroutes api-routes
-  (GET "/" [] "Hello World!!!")
+  (GET "/" [] (api/hello-world))
   (route/resources "/")
   (route/not-found const/four-oh-four-message))
