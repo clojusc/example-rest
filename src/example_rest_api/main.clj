194a93d4b629abd74c889cc92b6990beaf491ad1
Duncan McGreggor <duncan.mcgreggor@rackspace.com>
2013-09-29 18:24:21 -0500

    Removed clj-logging-config.

--- src/example_rest_api/main.clj
+++ src/example_rest_api/main.clj
@@ -1,17 +1,10 @@
 (ns example-rest-api.main
   (:require [clojure.tools.logging :as log]
-            [clj-logging-config.log4j :as log-config]
             [example-rest-api.config :as config]
             [example-rest-api.server :as server]
             [example-rest-api.util :as util])
   (:gen-class))
 
-
-(log-config/set-logger!
-  "Example REST API Logger"
-  :name "api-log"
-  :pattern "%d - %m%n"
-  :out (org.apache.log4j.ConsoleAppender.))
 
 (defn -main
   "This is the entry point.
@@ -23,7 +16,6 @@
     (when (util/show-help? options)
       (println banner)
       (System/exit 0))
-
     (if (util/in-dev? options)
        (server/start-dev server-manager :port config/default-port)
       (server/start server-manager))))
