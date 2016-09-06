8a607a60e15c4cd7bd710e5f0ab4aa526626e8c2
Duncan McGreggor <oubiwann@gmail.com>
2016-09-06 16:54:55 -0500

    Added more API functions (and updated routes).

--- src/example_rest/client/api.clj
+++ src/example_rest/client/api.clj
@@ -6,3 +6,26 @@
 
 (defn get-order [base-url order-id & args]
   (http/get (format "%s/order/%s" base-url order-id) args))
+
+(defn new-order [base-url & args]
+  (http/post (format "%s/order" base-url) args))
+
+(defn update-order [base-url order-id status & args]
+  (http/put
+    (format "%s/order/%s/%s" base-url order-id (name status)) args))
+
+(defn cancel-order [base-url order-id & args]
+  (http/delete (format "%s/order/%s" base-url order-id) args))
+
+(defn get-order-options [base-url order-id & args]
+  (http/options (format "%s/order/%s" base-url order-id) args))
+
+(defn update-payment [base-url order-id status & args]
+  (http/put
+    (format "%s/payment/order/%s/%s" base-url order-id (name status)) args))
+
+(defn get-payment [base-url order-id & args]
+  (http/get (format "%s/payment/order/%s" base-url order-id) args))
+
+(defn get-payment-options [base-url order-id & args]
+  (http/options (format "%s/payment/order/%s" base-url order-id) args))
--- src/example_rest/server/routes.clj
+++ src/example_rest/server/routes.clj
@@ -4,12 +4,21 @@
             [compojure.route :as route]))
 
 (defroutes v1
-  (GET "/orders" [] "<h1>All Current Orders:</h1>")
-  (POST "/order" [] "<h1>Order placed.</h1>")
-  (GET "/order/:id" [id] (str "<h2>Order " id " is not ready.</h2>"))
-  (PUT "/order/:id" [id] (str "<h2>Received update for Order " id "</h2>"))
-  (DELETE "/order/:id" [id] (str "<h2>All done with Order " id "</h2>"))
-  (OPTIONS "/order/:id" [id] (str "<h2>That order is allowed to...</h2>"))
-  (GET "/payment/order/:id" [id] (str "<h2>Payment Status</h2>"))
-  (PUT "/payment/order/:id" [id] (str "<h2>Paid for Order " id "</h2>"))
-  (OPTIONS "/payment/order/:id" [id] "<h2>That payment can be...</h2>"))
+  (GET "/orders" []
+    "<h1>All Current Orders:</h1>")
+  (POST "/order" []
+    "<h1>Order placed.</h1>")
+  (GET "/order/:id" [id]
+    (str "<h2>Order " id " is not ready.</h2>"))
+  (PUT "/order/:id/:status" [id status]
+    (str "<h2>Order " id " is now " status "</h2>"))
+  (DELETE "/order/:id" [id]
+    (str "<h2>Order " id " has been cancelled.</h2>"))
+  (OPTIONS "/order/:id" [id]
+    (str "<h2>That order is allowed to...</h2>"))
+  (GET "/payment/order/:id" [id]
+    (str "<h2>Payment Status</h2>"))
+  (PUT "/payment/order/:id/:status" [id status]
+    (str "<h2>Status for Order " id ": " status " </h2>"))
+  (OPTIONS "/payment/order/:id" [id]
+    "<h2>That payment can be...</h2>"))
