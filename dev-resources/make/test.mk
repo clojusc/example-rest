63611553eb04142827739ee945ab6d81c713ddae
Duncan McGreggor <oubiwann@gmail.com>
2016-09-06 15:16:23 -0500

    Added more testing targets.

--- dev-resources/make/test.mk
+++ dev-resources/make/test.mk
@@ -1,6 +1,10 @@
-lint:
-	@lein kibit
-	@lein eastwood "{:namespaces [:source-paths]}"
+kibit:
+	@lein with-profile +test kibit
+
+eastwood:
+	@lein with-profile +test eastwood "{:namespaces [:source-paths]}"
+
+lint: kibit eastwood
 
 lint-unused:
 	@lein eastwood "{:linters [:unused-fn-args :unused-locals :unused-namespaces :unused-private-vars :wrong-ns-form] :namespaces [:source-paths]}"
@@ -8,5 +12,16 @@ lint-unused:
 lint-ns:
 	@lein eastwood "{:linters [:unused-namespaces :wrong-ns-form] :namespaces [:source-paths]}"
 
+unit:
+	@lein test :unit
+
+system:
+	@lein test :system
+
+integration:
+	@lein test :integration
+
 check: lint
-	lein test
+	@lein test :all
+
+.PHONY: check lint
