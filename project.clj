e383f02527152553ea3ec5ba1ad90004c4dffef4
Duncan McGreggor <oubiwann@gmail.com>
2016-09-06 15:15:46 -0500

    More deps, updates, etc.

--- project.clj
+++ project.clj
@@ -8,20 +8,22 @@
     :name "Apache License, Version 2.0"
     :url "http://www.apache.org/licenses/LICENSE-2.0"}
   :dependencies [
+    [clj-http "3.2.0"]
+    [clojusc/twig "0.2.3"]
+    [com.stuartsierra/component "0.3.1"]
+    [compojure "1.5.1"]
+    [dire "0.5.4"]
+    [http-kit "2.2.0"]
+    [leiningen-core "2.7.0" :exclusions [org.clojure/clojure]]
     [org.clojure/clojure "1.8.0"]
     [org.clojure/tools.cli "0.3.5"]
-    [clojusc/twig "0.2.3"]
-    [clj-http "3.2.0"]
-    [compojure "1.5.1"]
-    [http-kit "2.2.0"]
-    [com.stuartsierra/component "0.3.1"]
-    [ring.middleware.logger "0.5.0" :exclusions [org.slf4j/slf4j-log4j12]]
+    [ring.middleware.logger "0.5.0"]
     [ring/ring-core "1.5.0"]
     [ring/ring-defaults "0.2.1"]
     [ring/ring-devel "1.5.0"]
     [ring/ring-jetty-adapter "1.5.0"]
-    [ring/ring-json "0.4.0"]
-    [leiningen-core "2.7.0"]]
+    [ring/ring-json "0.4.0"]]
+  :source-paths ["src"]
   :main example-rest.server.app
   :rest-server {
     :host "localhost"
@@ -30,9 +32,12 @@
     :level :info
     :namespaces [example-rest ring onelog]}
   :profiles {
-    :uber {
+    :uberjar {
       :aot :all}
     :test {
+      :plugins [
+        [jonase/eastwood "0.2.3" :exclusions [org.clojure/clojure]]
+        [lein-kibit "0.1.2" :exclusions [org.clojure/clojure]]]
       :test-selectors {
       :default :unit
       :unit :unit
@@ -51,7 +56,10 @@
           :doc "Documentation forthcoming"}}}
     :dev {
       :dependencies [
-        [org.clojure/tools.namespace "0.2.11"]]
+        [org.clojure/tools.namespace "0.3.0-alpha3" :exclusions [
+          org.clojure/clojure org.clojure/tools.reader]]
+        [org.clojure/tools.nrepl "0.2.12"]]
       :plugins [[lein-ring "0.9.7"]]
       :source-paths ["dev-resources/src"]
-      :repl-options {:init-ns example-rest.dev}}})
+      :repl-options {:init-ns example-rest.dev}
+      }})
